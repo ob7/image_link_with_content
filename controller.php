fe6c073a2e9b9d8ec6be3774592e3c82b7f10247
OB7 Dev <dp@ob7.us>
2016-09-05 21:37:15 -0800

    Updated controller version

--- controller.php
+++ controller.php
@@ -7,7 +7,7 @@ class Controller extends Package
 {
     protected $pkgHandle = 'image_link_with_content';
     protected $appVersionRequired = '5.7.5.1';
-    protected $pkgVersion = '0.9.9';
+    protected $pkgVersion = '1.0';
 
     public function getPackageName()
     {
